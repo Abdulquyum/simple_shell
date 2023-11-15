41876134ae93c01a910cb89d6ea5022f94ef423b
sophie-analytics <sonibaresofiat@gmail.com>
2023-11-15 20:45:31 +0100

    rewrote the code to correct some errors

--- shell.c
+++ shell.c
@@ -7,19 +7,32 @@
 #include <fcntl.h>
 #include "shell.h"
 
+/**
+ * shell_prompt - command to prints the shell prompt
+ *
+ * Return: exit
+ */
+
 char *prompt = "Shell$ ";
 
 void shell_prompt()
 {
 	if (isatty(STDIN_FILENO))
 	{
-    	if (write(STDOUT_FILENO, prompt, strlen(prompt)) == -1)
-    	{
-        	perror("write");
-        	exit(EXIT_FAILURE);
-    	}
+		if (write(STDOUT_FILENO, prompt, strlen(prompt)) == -1)
+		{
+			perror("write");
+			exit(EXIT_FAILURE);
+		}
 	}
 }
+
+/**
+ * execute_command - function that accepts and execute a command
+ * @command: command to execute
+ *
+ * Return: Sucess
+ */
 
 int execute_command(const char *command)
 {
@@ -29,42 +42,42 @@ int execute_command(const char *command)
 
 	if (strcmp(command, "exit") == 0)
 	{
-    	exit(EXIT_SUCCESS);
+		exit(EXIT_SUCCESS);
 	}
 	else if (strcmp(command, "env") == 0)
 	{
-    	extern char **environ;
-    	for (i = 0; environ[i] != NULL; i++)
-    	{
-        	printf("%s\n", environ[i]);
-    	}
-    	return 0;
+		extern char **environ;
+		for (i = 0; environ[i] != NULL; i++)
+		{
+			printf("%s\n", environ[i]);
+		}
+		return (0);
 	}
 	else
 	{
-    	child_pid = fork();
-    	if (child_pid == -1)
-    	{
-        	perror("fork");
-        	return -1;
-    	}
-    	else if (child_pid == 0)
-    	{
-        	execlp(command, command, (char *)NULL);
-        	fprintf(stderr, "%s: command not found\n", command);
-        	_exit(EXIT_FAILURE);
-    	}
-    	else
-    	{
-        	waitpid(child_pid, &status, 0);
-        	if (WIFEXITED(status))
-        	{
-            	return WEXITSTATUS(status);
-        	}
-        	else
-        	{
-            	return -1;
-        	}
-    	}
+		child_pid = fork();
+		if (child_pid == -1)
+		{
+			perror("fork");
+			return (-1);
+		}
+		else if (child_pid == 0)
+		{
+			execlp(command, command, (char *)NULL);
+			fprintf(stderr, "%s: command not found\n", command);
+			_exit(EXIT_FAILURE);
+		}
+		else
+		{
+			waitpid(child_pid, &status, 0);
+			if (WIFEXITED(status))
+			{
+				return (WEXITSTATUS(status));
+			}
+			else
+			{
+				return (-1);
+			}
+		}
 	}
 }
--- source.c
+++ source.c
@@ -6,6 +6,12 @@
 #include <sys/wait.h>
 #include "shell.h"
 
+/**
+ * main - Entry point
+ *
+ * Return: Success
+ */
+
 int main(void)
 {
 	char *line_ptr = NULL;
@@ -18,66 +24,56 @@ int main(void)
 
 	while (1)
 	{
-    	shell_prompt();
+		shell_prompt();
 
-    	read_input = getline(&line_ptr, &buffer, stdin);
+		read_input = getline(&line_ptr, &buffer, stdin);
+		if (read_input == -1)
+		{
+			if (feof(stdin))
+			{
+				break;
+			}
+			else
+			{
+				perror("Error");
+				exit(EXIT_FAILURE);
+			}
+		}
+		token = strtok(line_ptr, " \t\n");
+		arg_count = 0;
 
-    	if (read_input == -1)
-    	{
-        	if (feof(stdin))
-        	{
-            	break;
-        	}
-        	else
-        	{
-            	perror("Error");
-            	exit(EXIT_FAILURE);
-        	}
-    	}
-
-    	token = strtok(line_ptr, " \t\n");
-    	arg_count = 0;
-
-    	while (token != NULL)
-    	{
-        	args[arg_count++] = token;
-        	token = strtok(NULL, " \t\n");
-    	}
-
-    	if (arg_count == 0)
-    	{
-        	continue; // No command entered, prompt again
-    	}
-
-    	args[arg_count] = NULL; // Null-terminate the argument list
-
-    	if (strcmp(args[0], "exit") == 0)
-    	{
-        	free(line_ptr);
-        	exit(EXIT_SUCCESS);
-    	}
-
-    	child_pid = fork();
-
-    	if (child_pid == -1)
-    	{
-        	perror("Error");
-        	_exit(EXIT_FAILURE);
-    	}
-
-    	if (child_pid == 0)
-    	{
-        	execvp(args[0], args);
-        	fprintf(stderr, "%s: No such file or directory\n", args[0]);
-        	_exit(EXIT_FAILURE);
-    	}
-    	else
-    	{
-        	wait(&wstatus);
-    	}
-
-    	free(line_ptr);
+		while (token != NULL)
+		{
+			args[arg_count++] = token;
+			token = strtok(NULL, " \t\n");
+		}
+		if (arg_count == 0)
+		{
+			continue;
+		}
+		args[arg_count] = NULL;
+		if (strcmp(args[0], "exit") == 0)
+		{
+			free(line_ptr);
+			exit(EXIT_SUCCESS);
+		}
+		child_pid = fork();
+		if (child_pid == -1)
+		{
+			perror("Error");
+			_exit(EXIT_FAILURE);
+		}
+		if (child_pid == 0)
+		{
+			execvp(args[0], args);
+			fprintf(stderr, "%s: No such file or directory\n", args[0]);
+			_exit(EXIT_FAILURE);
+		}
+		else
+		{
+			wait(&wstatus);
+		}
+		free(line_ptr);
 	}
-
 	_exit(EXIT_SUCCESS);
 }
